fdfecbcec3b55e18f77b608eaf225b91650dfe86
GITMAN Alexandru <alexandru.gitman@ext.ec.europa.eu>
2024-10-21 16:10:03 +0200

    Export to PDF

--- scripts.js
+++ scripts.js
@@ -1,77 +1,139 @@
 
+const { PDFDocument, StandardFonts, rgb } = PDFLib
+const fontSize = 7;
+
+async function generatePDF() {
+    const pdfDoc = await PDFLib.PDFDocument.create();
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+    const page = pdfDoc.addPage([210, 297]);
+
+    page.drawText('Name: .....................', {
+        x: 10,
+        y: 297 - fontSize - 10,
+        size: fontSize,
+        font: helveticaFont
+      });        
+
+      page.drawText('Date: ....../......./20......', {
+        x: 130,
+        y: 297 - fontSize - 10,
+        size: fontSize,
+        font: helveticaFont
+      });        
+
+    let bag = fillEquations(getRequest());
+
+    let yOffset = 50;
+    let xOffset = 10;
+    let coll = 0;
+    for (let i of bag) {
+        page.drawText(i.a + ' ' + i.op + ' ' + i.b + ' = ____ ', {
+            x: xOffset,
+            y: 297 - fontSize - yOffset,
+            size: fontSize,
+            font: helveticaFont,
+            color: rgb(0, 0.53, 0.71),
+          });        
+        xOffset+=70;          
+
+        coll++;
+        if(coll>2) {
+            coll = 0;
+            yOffset = yOffset + 15;
+            xOffset = 10;
+        }
+        
+    }
+
+    page.drawText('Generated at: https://aegitman.github.io/math', {
+        x: 10,
+        y: 15,
+        size: 5,
+        font: helveticaFont
+      });   
+
+    
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    
+    const linkSource = 'data:application/pdf;base64' + pdfDataUri;
+    const downloadLink = document.createElement("a");
+    const fileName = "abc.pdf";
+    downloadLink.href = linkSource;
+    downloadLink.download = fileName;
+    downloadLink.click();
+}
 
 function generateTable(evt) {
+  let $t = $("#table");
 
-    let obj = getRequest();
+  $t.empty();
 
-    let $t = $('#table');
+  let bag = fillEquations(getRequest());
 
-    $t.empty();
-
-    let bag = fillEquations(obj);
-
-
-    for(let i of bag) {
-        $t.append($('<div>').append(generateTd(i)));
-    }    
+  for (let i of bag) {
+    $t.append($("<div>").append(generateTd(i)));
+  }
 }
 
 function getRequest() {
-    let numbers = [];
+  let operation = $("#operationSel").find("option:selected").val();
+  let numbers = [];
+  $("div.checkboxDiv input[type=checkbox]").each(function () {
+    if ($(this).is(":checked")) {
+      numbers.push($(this).attr("data-value"));
+    }
+  });
 
-    $('div.checkboxDiv input[type=checkbox]').each(function() {
-       if ($(this).is(":checked")) {
-            numbers.push($(this).attr('data-value'));
-       }
-    });
+  if (numbers.length == 0 || operation == undefined) {
+    return undefined;
+  }
 
-     return {'n': numbers, 'a': $('#operationSel').find('option:selected').val(), 'q': $('#quantity').val()};
+  return { n: numbers, a: operation, q: $("#quantity").val() };
 }
 
-
 function generateTd(bag) {
-    let $eq = $("<span>").addClass('eqSpan').text(bag.a + " " + bag.op + " "+ bag.b + " = ");
+  let a = bag.a;
+  let b = bag.b;
+  // subtraction above 0
+  if (bag.op == "-" && b > a) {
+    a = bag.b;
+    b = bag.a;
+  }
+  let $eq = $("<span>")
+    .addClass("eqSpan")
+    .text(a + " " + bag.op + " " + b + " = ");
 
-    return $("<div>").addClass('equationDiv').append($eq);
+  return $("<div>").addClass("equationDiv").append($eq);
 }
 
 function fillEquations(obj) {
-    let objCount = Number(obj.n.length);
-    // in case we do not want repetitions
-    let thMax = objCount * 12;
+  if (obj == undefined) {
+    return [];
+  }
 
-    let bag = [];
-    let max = Number(obj.q) < 10 ? 10 : Number(obj.q);
-    max = max > 40 ? 40 : max;
-    
+  let objCount = Number(obj.n.length);
 
-    for(let m=0; m < Math.floor(max/objCount); m++) {
-        // add at least one equation with every number     
-        for(let i of obj.n) {
-            bag.push({a:i, b:randomTo12(), op:obj.a[0]});
-        }
+  let bag = [];
+  let max = Number(obj.q) < 12 ? 12 : Number(obj.q);
+  max = max > 40 ? 40 : max;
+
+  for (let m = 0; m < Math.floor(max / objCount); m++) {
+    // add at least one equation with every number
+    for (let i of obj.n) {
+      bag.push({ a: i, b: randomTo12(), op: obj.a[0] });
     }
+  }
 
-    for(let m=0; m < Math.floor(max%objCount); m++) {
-        // add the rest of the equations with every number         
-        bag.push({a:obj.n[Math.floor(Math.random() * obj.n.length)], b:randomTo12(), op:obj.a[0]});        
-    }
+  for (let m = 0; m < Math.floor(max % objCount); m++) {
+    // add the rest of the equations with every number
+    bag.push({
+      a: obj.n[Math.floor(Math.random() * obj.n.length)],
+      b: randomTo12(),
+      op: obj.a[0],
+    });
+  }
 
-    return bag;
-}
-
-
-function validateUniqueness(bog){
-
-    let unique = []
-
-    for(let b of bog) {
-        for(let i in unique) {
-
-        }
-    }
-
-
+  return bag;
 }
 
 /**
@@ -79,6 +141,6 @@ function validateUniqueness(bog){
  *
  * @returns {number} A random integer between 1 and 12.
  */
-function randomTo12(){
-    return Math.floor(Math.random() * 12 + 1);
+function randomTo12() {
+  return Math.floor(Math.random() * 12 + 1);
 }
